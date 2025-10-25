1bf6fea4f0109c10309f7d5e1cf5fdcd1e90a0eb
Archie To <toanhminh0412@gmail.com>
2025-10-25 14:48:48 -0700

    Create Django models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+# NOTES:
+# - null=False, blank=False are by default but I want to be specific
+class Category(models.Model):
+    """
+    Product categories
+    Assuming one product falls into one category only
+    """
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+class Tag(models.Model):
+    """
+    Product tags
+    Assuming one product can have multiple tags and one tag
+    can belong to multiple products
+    """
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+class Product(models.Model):
+    """
+    Individual products
+    """
+    # NOTES:
+    # - Products should include images but exempted here as it's not mentioned
+    # in the assignment description
+    name = models.CharField(max_length=200, null=False, blank=False)
+    price = models.FloatField(null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    tag = models.ManyToManyField(Tag)
