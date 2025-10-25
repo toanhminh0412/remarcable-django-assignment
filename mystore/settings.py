c014df3e32beb96c5ad3fd85cbda4fd03b474a0e
Archie To <toanhminh0412@gmail.com>
2025-10-25 14:58:36 -0700

    Load sensitive and configurable data using env vars

--- mystore/settings.py
+++ mystore/settings.py
@@ -10,23 +10,33 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load env vars from .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--u!%6q6(^k%wx!*-f*t-_1$5(yws-ju&m5i&zkvr@_-t+1*f=5'
+# Use os.environ here to force setting a SECRET_KEY in production
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG').lower() == 'true'
 
 ALLOWED_HOSTS = []
-
+if hosts_str := os.getenv('ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = [
+        host.strip()
+        for host in hosts_str.split(',')
+        if host.strip()
+    ]
 
 # Application definition
 
