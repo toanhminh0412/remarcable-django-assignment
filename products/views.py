d82a9723d94bf0f1a431ec8a2ee8eb011dcdb3f2
Archie To <toanhminh0412@gmail.com>
2025-10-25 16:18:31 -0700

    Render products in a table

--- products/views.py
+++ products/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import TemplateView
+from .models import Category, Tag, Product
 
 class IndexView(TemplateView):
     """
     Index page: /
     """
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = (
+            Product.objects.all().prefetch_related('tag').select_related('category')
+        )
+        return context
